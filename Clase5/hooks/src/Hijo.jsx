da80b464640f21846cad937d3392c8fe65a4fd47
lautiiperez <perezlautaro@hotmail.com>
2023-08-07 21:59:29 -0300

    agrego practicaC5

---
+++ Clase5/hooks/src/Hijo.jsx
@@ -0,0 +1,16 @@
+function Hijo({count, setCount}){
+return(
+    <div>
+<h2>El contador esta en: {count}</h2>
+<button onClick={()=>setCount((count)=>count+1)}>Incrementar</button>
+
+<button onClick={()=>setCount(0)}>Reset</button>
+
+    </div>
+)
+
+
+
+}
+
+export default Hijo;
