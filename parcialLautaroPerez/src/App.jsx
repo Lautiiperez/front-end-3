6a678e595fded68d36ebf27a460f592ce182a03d
lautiiperez <perezlautaro@hotmail.com>
2023-08-14 21:18:54 -0300

    css

--- parcialLautaroPerez/src/App.jsx
+++ parcialLautaroPerez/src/App.jsx
@@ -1,11 +1,15 @@
 import Card from "./Card";
+import './App.css';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Carga de estudiantes</h1>
-      <form></form>
+      <form>
+
+      
+      </form>
       <Card />
     </div>
   );
